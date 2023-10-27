162412332488a5ec95bcd20c242b950dcff27691
Emplexx <ishchuk132@gmail.com>
2023-10-27 22:25:26 +0200

    🚀🚀🚀🚀

--- app/src/main/java/moe/emi/finite/service/db/SubscriptionDao.kt
+++ app/src/main/java/moe/emi/finite/service/db/SubscriptionDao.kt
@@ -1,7 +1,6 @@
 package moe.emi.finite.service.db
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
